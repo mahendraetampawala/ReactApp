c99e0b82b967c3d9564ca42d768ae5e47bd638c7
mahendraetampawala <mahendraetampawala98@gmail.com>
2021-07-28 22:35:55 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+       
+
+        <View style={styles.carcontainer}>
+           <ImageBackground source={require('./assets/images/bmw.jpeg')}style={styles.image}/>
+          <View style={styles.titles}>
+          <Text style={styles.title}>BMW paradise</Text>
+          <Text style={styles.subtitle}>DESIGNED BY</Text>
+          <Text style={styles.subtitle}>MAHENDRA ETAMPAWALA</Text>
+          </View>
+        </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +27,35 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  carcontainer: {
+    width:'100%',
+    height:'100%',
+
+  },
+  titles: {
+    marginTop:'20%',
+    width:'100%',
+    alignItems:'center',
+
+  },
+  title: {
+    fontSize:40,
+    fontWeight:'600',
+    color:'#FFFAFA',
+  },
+  subtitle: {
+    fontSize:16,
+    color:'#AFEEEE',
+  },
+  image:{
+    width:'100%',
+    height:'100%',
+    resizeMode:'cover',
+    position:'absolute',
+
+  }
+
+
+
 });
